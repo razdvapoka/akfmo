573e46c1533121ab8048e077614c413924bdc0ee
razdvapoka <razdvapoka@gmail.com>
2021-10-01 00:36:27 +0300

    :construction: Update lang switcher

--- components/lang-switcher.js
+++ components/lang-switcher.js
@@ -1,17 +1,22 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import cn from 'classnames'
 
 export default function LangSwitcher() {
   const { locale: currentLocale, locales, asPath } = useRouter()
   return (
     <div className="space-x-2">
-      {locales
-        .filter((locale) => locale !== currentLocale)
-        .map((locale) => (
-          <Link key={locale} href={asPath} locale={locale} scroll={false}>
-            <a>{locale}</a>
-          </Link>
-        ))}
+      {locales.map((locale) => (
+        <Link key={locale} href={asPath} locale={locale} scroll={false}>
+          <a
+            className={cn({
+              'pointer-events-none underline': locale === currentLocale,
+            })}
+          >
+            {locale}
+          </a>
+        </Link>
+      ))}
     </div>
   )
 }
