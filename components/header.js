2f059c326be59df15de912026c9c30a811733491
Taras Senko <48488576+KolinRobinson@users.noreply.github.com>
2021-10-06 00:08:56 +0300

    Menu/header (#2)
    * update(header): develop
    
    * update(header): fix position
    
    * update(header): fix menu export and fix padding
    
    * update(header): fix after review
    
    Co-authored-by: razdvapoka <razdvapoka@gmail.com>

--- components/header.js
+++ components/header.js
@@ -1,9 +1,10 @@
-import { Logo } from './logo'
+import { Logo, Menu } from '.'
 
 export const Header = () => {
   return (
-    <header className="bg-white z-50 fixed top-0 left-0 w-screen p-4">
+    <header className="bg-white z-50 relative top-0 left-0 w-screen px-4 pt-6 ">
       <Logo />
+      <Menu />
     </header>
   )
 }
--- components/logo/index.js
+++ components/logo/index.js
@@ -6,7 +6,10 @@ export const Logo = () => {
   let isDarkTheme = false // future: page past events
   return (
     <Link href="/">
-      <a className="flex justify-center items-center" aria-label="logotype">
+      <a
+        className="flex justify-center items-center mb-12"
+        aria-label="logotype"
+      >
         <LogoSvg
           className={cn('w-[35.5rem]', {
             'stroke-white text-white': isDarkTheme,
