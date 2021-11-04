eb5164182f83bf2a3d94943313ca63d406ab77e1
Kolin Robinson <taras.senko.99@gmail.com>
2021-11-04 16:25:22 +0300

    fix after review

--- components/event-info.js
+++ components/event-info.js
@@ -9,16 +9,16 @@ export const EventInfo = ({ event }) => {
     [event.date]
   )
   return (
-    <div className="flex h-20 text-m leading-ml uppercase mb-16">
-      <div className="pl-2 border-l flex flex-col justify-between w-[24rem]">
+    <div className="flex h-20 text-m leading-ml uppercase mb-16 space-x-2 w-7/10">
+      <div className="pl-2 border-l flex flex-col justify-between w-1/3">
         <h2 className="font-bold"> {t('eventInfo.date')}</h2>
         <time dateTime={event.date}>{dateString}</time>
       </div>
-      <div className="ml-2 pl-2 border-l flex flex-col justify-between w-[24rem]">
+      <div className="pl-2 border-l flex flex-col justify-between w-1/3">
         <h2 className="font-bold">{t('eventInfo.format')}</h2>
-        <p>{event.format || 'in development'}</p>
+        <p>{event.format}</p>
       </div>
-      <div className="ml-2 pl-2 border-l flex flex-col justify-between w-[24rem]">
+      <div className="pl-2 border-l flex flex-col justify-between w-1/3">
         <h2 className="font-bold">{t('eventInfo.location')}</h2>
         <p>{event.location}</p>
       </div>
