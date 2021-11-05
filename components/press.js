9977e1c7b73c1f54c558a1d39b50966b7a6d90e1
Kolin Robinson <taras.senko.99@gmail.com>
2021-11-05 13:02:59 +0300

    Develop section pressa

---
+++ components/press.js
@@ -0,0 +1,18 @@
+import useTranslation from 'next-translate/useTranslation'
+import { SinglePressa } from '.'
+
+export const Press = ({ press }) => {
+  const { t } = useTranslation('common')
+  return (
+    <section className="border-t mb-16 pt-2">
+      <h2 className="font-bold uppercase text-m leading-l mb-10">
+        {t('press.title')}
+      </h2>
+      <ul>
+        {press.map((item, index) => (
+          <SinglePressa pressa={item.item} key={index} />
+        ))}
+      </ul>
+    </section>
+  )
+}
