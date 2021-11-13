e5d117efc9a2eea15ea913fd333eda63c2be2666
Taras Senko <48488576+KolinRobinson@users.noreply.github.com>
2021-11-13 22:34:57 +0200

    develop publication adaptive (#18)
    * develop publication adaptive
    
    * :art: Improve stuff
    
    Co-authored-by: razdvapoka <razdvapoka@gmail.com>

--- components/publications.js
+++ components/publications.js
@@ -1,14 +1,17 @@
 import Image from 'next/image'
-import { useState } from 'react'
-import { Publication } from '.'
+import { Fragment, useState } from 'react'
+import { Publication, PublicationMobile } from '.'
 
 export const Publications = ({ publications }) => {
   const [publicationIndex, setPublicationIndex] = useState(0)
   const publication = publications[publicationIndex]
 
   return (
-    <section className="flex pt-4">
-      <div className="pr-4 w-1/2">
+    <section className="flex pt-4 lg:flex-col lg:border-t lg:mb-6">
+      <h1 className="hidden lg:inline-flex items-center justify-center pb-4 border-b font-bold uppercase">
+        Publications
+      </h1>
+      <div className="pr-4 w-1/2 lg:hidden">
         <div className="py-4 border-t h-full">
           <div className="w-2/3 mx-auto sticky top-4">
             <Image
@@ -22,19 +25,21 @@ export const Publications = ({ publications }) => {
           </div>
         </div>
       </div>
-      <div className="w-1/2">
-        <div className="pl-4">
-          <div className="border-t"></div>
+      <div className="w-1/2 lg:w-full">
+        <div className="pl-4 lg:pl-0">
+          <div className="border-t lg:border-t-0"></div>
         </div>
-        <nav className="pt-4">
-          <ul className="border-l pl-4 pt-4">
+        <nav className="pt-4 lg:pt-0">
+          <ul className="border-l pl-4 pt-4 lg:border-l-0 lg:p-0">
             {publications.map((item, index) => (
-              <Publication
-                setPublicationIndex={setPublicationIndex}
-                index={index}
-                item={item}
-                key={index}
-              />
+              <Fragment key={index}>
+                <Publication
+                  setPublicationIndex={setPublicationIndex}
+                  index={index}
+                  item={item}
+                />
+                <PublicationMobile item={item} key={index} />
+              </Fragment>
             ))}
           </ul>
         </nav>
