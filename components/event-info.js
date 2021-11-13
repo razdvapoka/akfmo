3d26a127a3f25aef09d9c7d7d25e663c3357a501
razdvapoka <razdvapoka@gmail.com>
2021-11-13 23:46:36 +0300

    :hammer: Refactor EventInfo

--- components/event-info.js
+++ components/event-info.js
@@ -2,6 +2,15 @@ import { format } from 'date-fns'
 import { useMemo } from 'react'
 import useTranslation from 'next-translate/useTranslation'
 
+export const EventInfoItem = ({ title, children, date }) => {
+  return (
+    <div className="pl-2 border-l flex flex-col justify-between w-1/3 lg:border-0 lg:w-full lg:pl-0">
+      <h2 className="font-bold">{title}</h2>
+      {date ? <time dateTime={date}>{children}</time> : <p>{children}</p>}
+    </div>
+  )
+}
+
 export const EventInfo = ({ event }) => {
   const { t } = useTranslation('common')
   const dateString = useMemo(
@@ -9,19 +18,27 @@ export const EventInfo = ({ event }) => {
     [event.date]
   )
   return (
-    <div className="flex h-20 text-m leading-ml uppercase mb-16 space-x-2 w-7/10 lg:space-x-0 lg:space-y-8 lg:flex-col lg:h-auto lg:mb-8 lg:order-6">
-      <div className="pl-2 border-l flex flex-col justify-between w-1/3 lg:border-0 lg:w-full lg:pl-0">
-        <h2 className="font-bold"> {t('eventInfo.date')}</h2>
-        <time dateTime={event.date}>{dateString}</time>
-      </div>
-      <div className="pl-2 border-l flex flex-col justify-between w-1/3 lg:border-0  lg:w-full lg:pl-0">
-        <h2 className="font-bold">{t('eventInfo.format')}</h2>
-        <p>{event.format}</p>
-      </div>
-      <div className="pl-2 border-l flex flex-col justify-between w-1/3 lg:border-0  lg:w-full lg:pl-0">
-        <h2 className="font-bold">{t('eventInfo.location')}</h2>
-        <p>{event.location}</p>
-      </div>
+    <div
+      className={`
+        flex lg:flex-col
+        h-20 lg:h-auto
+        w-7/10
+        text-m leading-ml uppercase
+        mb-16 lg:mb-8
+        space-x-2 lg:space-x-0
+        lg:space-y-8
+        lg:order-6
+      `}
+    >
+      <EventInfoItem title={t('eventInfo.date')} date={event.date}>
+        {dateString}
+      </EventInfoItem>
+      <EventInfoItem title={t('eventInfo.format')}>
+        {event.format}
+      </EventInfoItem>
+      <EventInfoItem title={t('eventInfo.location')}>
+        {event.location}
+      </EventInfoItem>
     </div>
   )
 }
