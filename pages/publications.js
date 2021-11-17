248383211eb886a36ae2fae91bc5ee797d5b2013
razdvapoka <razdvapoka@gmail.com>
2021-11-18 00:56:30 +0300

    :alien: Update publications api

--- pages/publications.js
+++ pages/publications.js
@@ -1,40 +1,28 @@
 import Head from 'next/head'
-import { useMemo } from 'react'
-import { repeat } from '../lib/utils'
 import { getPublications } from '../lib/api'
 import { Layout, Publications } from '../components'
 
 export default function PublicationsPage({ data }) {
-  const publications = useMemo(
-    () =>
-      repeat(
-        10,
-        data.publications.map(({ publication }) => publication)
-      )
-        .flat()
-        .map((p, i) => ({
-          ...p,
-          title: `${p.title}—(${i + 1})`,
-          year: p.year + i,
-        })),
-    [data]
-  )
-
   return (
     <Layout>
       <Head>
         <title>AKFMO Publications</title>
       </Head>
 
-      <Publications publications={publications}></Publications>
+      <Publications publications={data}></Publications>
     </Layout>
   )
 }
 
 export async function getStaticProps(context) {
   const data = await getPublications(context.locale)
+  console.log(data.publications_page.publications)
   return {
-    props: { data: data.main },
+    props: {
+      data: data.publications_page.publications.map(
+        ({ publication }) => publication
+      ),
+    },
     revalidate: 60,
   }
 }
