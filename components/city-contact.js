1fb29c82f8bc367e082d09c04cdc05ed8f37f517
razdvapoka <razdvapoka@gmail.com>
2021-11-22 00:15:07 +0300

    :lipstick: Adapt contact page

--- components/city-contact.js
+++ components/city-contact.js
@@ -1,29 +1,35 @@
 import { ButtonLink } from '.'
 import useTranslation from 'next-translate/useTranslation'
 import AboutRect from '../assets/svg/about-rect.svg'
+import AboutRectMob from '../assets/svg/about-rect-m.svg'
 
 export const CityContact = () => {
   const { t } = useTranslation('common')
   return (
-    <div className="flex pb-4 border-t">
-      <div className="w-1/2 pr-4">
-        <div className="py-4">
-          <AboutRect />
+    <div className="flex pb-4 border-t lg:flex-col-reverse lg:pb-0">
+      <div className="w-1/2 pr-4 lg:w-full lg:pr-0 lg:border-t">
+        <div className="py-4 lg:pt-6 lg:pb-0">
+          <div className="lg:hidden">
+            <AboutRect />
+          </div>
+          <div className="hidden lg:block">
+            <AboutRectMob />
+          </div>
         </div>
       </div>
-      <div className="w-1/2 pt-4">
-        <div className="pl-4 pb-4 text-m leading-m border-l h-full">
-          <h2 className="uppercase font-bold mb-10">
+      <div className="w-1/2 pt-4 lg:w-full lg:pt-6">
+        <div className="pl-4 pb-4 text-m leading-m border-l h-full lg:border-l-0 lg:pl-0 lg:pb-6">
+          <h2 className="uppercase font-bold mb-10 lg:text-m lg:mb-6">
             {t('contactCity.title')}
           </h2>
-          <adress className="w-2/4 inline-block text-xl leading-l font-medium mb-12">
+          <adress className="w-1/2 inline-block text-xl leading-l font-medium mb-12 lg:text-m lg:w-3/4 lg:mb-4">
             {t('contactCity.adress')}
             <br />
             {t('contactCity.tel') + ' ' + t('contactCity.telNumber')}
             <br />
             {t('contactCity.fax') + ' ' + t('contactCity.faxNumber')}
           </adress>
-          <div className="flex justify-between h-24 space-x-8">
+          <div className="flex justify-between h-24 space-x-8 lg:h-10 lg:space-x-2">
             <ButtonLink
               link={'mailto:' + t('contactCity.mail')}
               className="w-1/2"
--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,10 @@ export default function Contact() {
       <Head>
         <title>AKFMO: Contact Us</title>
       </Head>
-      <section className="pt-4 pb-8">
+      <section className="pt-4 pb-8 lg:pt-0 lg:pb-6">
+        <h1 className="hidden lg:flex border-t py-4 items-center justify-center text-m uppercase font-bold">
+          contact us
+        </h1>
         <LocaleContact />
         <CityContact />
       </section>
