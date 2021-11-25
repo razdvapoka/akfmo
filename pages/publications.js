1f6abd35383534382b9a8fb47f27d5f972bf6a9d
razdvapoka <razdvapoka@gmail.com>
2021-11-25 15:22:07 +0300

    :sparkles: Sort publications by year

--- pages/publications.js
+++ pages/publications.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import { getPublications } from '../lib/api'
 import { Layout, Publications } from '../components'
+import { useMemo } from 'react'
 
 export default function PublicationsPage({ data }) {
+  const publications = useMemo(
+    () => data.sort((p1, p2) => (p1.year < p2.year ? 1 : -1)),
+    [data]
+  )
   return (
     <Layout>
       <Head>
         <title>AKFMO Publications</title>
       </Head>
 
-      <Publications publications={data}></Publications>
+      <Publications publications={publications}></Publications>
     </Layout>
   )
 }
