994143968da0842bfb6a19357469e4493100972f
razdvapoka <razdvapoka@gmail.com>
2021-11-29 15:33:27 +0300

    :lipstick: Update big event card

--- components/event-card-big.js
+++ components/event-card-big.js
@@ -14,44 +14,64 @@ export const EventCardBig = ({
   className,
 }) => {
   const dateString = useEventDateString(date, endDate)
+  const isLandscape = cover.dimensions.width > cover.dimensions.height
 
   return (
     <Link href={`/events/${uid}`}>
       <a
         className={cn(
-          'bg-pink py-8 grid grid-cols-24 lg:grid-cols-4 lg:py-2 lg:px-6 lg:relative',
+          'bg-pink py-8 grid grid-cols-24 h-[calc(100vh-4rem)] min-h-[50rem] lg:block lg:h-auto lg:min-h-0 lg:pt-2 lg:pb-8 lg:px-6',
           styles.eventCard,
           className
         )}
       >
-        <div className="col-start-5 col-end-20 uppercase font-bold text-m tracking-wider mb-4 lg:col-start-1 lg:col-span-full lg:mb-2 lg:text-xs">
-          {location} | {dateString}
-        </div>
-        <div className="flex col-start-5 col-end-22 lg:col-start-1 lg:col-span-full lg:flex-col lg:mb-1">
-          <div className="w-full">
-            <div className="aspect-w-1 aspect-h-1">
-              <Image
-                className={cn(
-                  'filter grayscale transition-all',
-                  styles.eventCardImage
-                )}
-                alt={cover.alt || title}
-                src={cover.url}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-              />
+        <div className="col-start-2 col-end-15 flex flex-col justify-between lg:block">
+          <div className="flex mb-10 lg:mb-0">
+            <div className="uppercase font-bold text-m tracking-wider mr-17 lg:mr-0 lg:text-xxs">
+              {location} | {dateString}
             </div>
+            <ul className="uppercase text-grey2 font-medium text-m tracking-wider flex space-x-4 lg:absolute lg:left-6 lg:bottom-1 lg:flex-wrap lg:space-x-1 lg:mb-1 lg:text-xxs">
+              {tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
           </div>
-          <ul className="ml-4 uppercase text-grey2 font-medium text-m tracking-wider space-y-[0.4rem] lg:flex lg:space-y-0 lg:space-x-1 lg:absolute lg:bottom-2 lg:text-xs lg:ml-0">
-            {tags.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
+          <div className="hidden lg:block my-2">
+            <Image
+              className="filter grayscale transition-all"
+              alt={cover.alt || title}
+              src={cover.url}
+              width={cover.dimensions.width}
+              height={cover.dimensions.height}
+              layout="responsive"
+            />
+          </div>
+          <h3 className="text-xxl font-medium leading-ml lg:text-ml">
+            {title}
+          </h3>
         </div>
-        <h3 className="text-xl font-medium leading-ml mt-2 col-span-11 col-start-5 lg:col-start-1 lg:col-span-full lg:w-full lg:p-0 lg:text-ml lg:pr-4 lg:pb-6 lg:mt-0">
-          {title}
-        </h3>
+        <div className="col-start-17 col-end-24 relative lg:hidden">
+          <Image
+            className={cn(
+              'filter grayscale transition-all',
+              styles.eventCardImage
+            )}
+            alt={cover.alt || title}
+            src={cover.url}
+            width={cover.dimensions.width}
+            height={cover.dimensions.height}
+            layout="fill"
+            {...(isLandscape
+              ? {
+                  objectFit: 'contain',
+                  objectPosition: 'bottom',
+                }
+              : {
+                  objectFit: 'cover',
+                  objectPosition: 'center',
+                })}
+          />
+        </div>
       </a>
     </Link>
   )
