be5a4533a06e57cb8044cbbc996764827056f393
razdvapoka <razdvapoka@gmail.com>
2021-11-29 19:52:24 +0300

    :lipstick: Update partners section

--- components/pages/event/partners.js
+++ components/pages/event/partners.js
@@ -5,11 +5,11 @@ export const Partners = ({ partners }) => {
   const { t } = useTranslation('common')
 
   return (
-    <section className="border-t mb-16 pt-2 col-start-2 col-end-20 lg:mt-4 lg:mb-4">
+    <section className="border-t mb-16 pt-2 col-start-2 col-end-18 lg:mt-4 lg:mb-4">
       <h2 className="font-bold uppercase text-m leading-l mb-10 lg:mb-6">
         {t('partners.title')}
       </h2>
-      <ul className="space-y-4 lg:space-y-6">
+      <ul className="flex flex-wrap">
         {partners
           .filter((item) => item.item)
           .map((item, index) => (
--- components/pages/event/single-partner.js
+++ components/pages/event/single-partner.js
@@ -3,19 +3,21 @@ import { RichText } from 'prismic-reactjs'
 
 export const SinglePartner = ({ partner }) => {
   return (
-    <li>
-      <div className="w-1/4 lg:w-1/2 mb-6 lg:mb-4">
-        <Image
-          src={partner.logo.url}
-          alt={partner.logo.alt}
-          layout="responsive"
-          objectFit="cover"
-          width={partner.logo.dimensions.width}
-          height={partner.logo.dimensions.height}
-        />
+    <li className="w-[calc(5/18*100%)] mr-10 mb-6 lg:w-full lg:mr-0 lg:mb-4">
+      <div className="filter grayscale border h-[15.6rem] flex justify-center items-center lg:w-full lg:h-[14rem]">
+        <div className="w-[16rem] h-17 lg:w-[14rem] lg:h-15 relative">
+          <Image
+            src={partner.logo.url}
+            alt={partner.logo.alt}
+            layout="fill"
+            objectFit="contain"
+            width={partner.logo.dimensions.width}
+            height={partner.logo.dimensions.height}
+          />
+        </div>
       </div>
-      <div className="w-3/5 text-l leading-l lg:w-full lg:text-m">
-        <RichText render={partner.description} />
+      <div className="text-m leading-l mt-2 uppercase font-bold lg:text-m">
+        <RichText render={partner.name} />
       </div>
     </li>
   )
