be887b794cf98963a83057cbaf7640bdcd810a59
razdvapoka <razdvapoka@gmail.com>
2022-01-20 18:06:59 +0300

    :sparkles: Add partner links

--- components/pages/event/single-partner.js
+++ components/pages/event/single-partner.js
@@ -4,21 +4,28 @@ import { RichText } from 'prismic-reactjs'
 export const SinglePartner = ({ partner }) => {
   return (
     <li className="w-[calc(5/18*100%)] mr-10 mb-6 lg:w-full lg:mr-0 lg:mb-4">
-      <div className="filter grayscale border h-[15.6rem] flex justify-center items-center lg:w-full lg:h-[14rem]">
-        <div className="w-[16rem] h-17 lg:w-[14rem] lg:h-15 relative">
-          <Image
-            src={partner.logo.url}
-            alt={partner.logo.alt}
-            layout="fill"
-            objectFit="contain"
-            width={partner.logo.dimensions.width}
-            height={partner.logo.dimensions.height}
-          />
+      <a
+        className="block"
+        href={partner.url.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="filter grayscale border h-[15.6rem] flex justify-center items-center lg:w-full lg:h-[14rem]">
+          <div className="w-[16rem] h-17 lg:w-[14rem] lg:h-15 relative">
+            <Image
+              src={partner.logo.url}
+              alt={partner.logo.alt}
+              layout="fill"
+              objectFit="contain"
+              width={partner.logo.dimensions.width}
+              height={partner.logo.dimensions.height}
+            />
+          </div>
         </div>
-      </div>
-      <div className="text-m leading-l mt-2 uppercase font-bold lg:text-m">
-        <RichText render={partner.name} />
-      </div>
+        <div className="text-m leading-l mt-2 uppercase font-bold lg:text-m">
+          <RichText render={partner.name} />
+        </div>
+      </a>
     </li>
   )
 }
